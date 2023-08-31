8eabdab92df764794af536228ff2bc929b4b1432
Gustavo Xudré <gxudre@gmail.com>
2023-08-31 20:33:32 -0300

    adicionando pratica02

---
+++ Praticas/pratica02/App.js
@@ -0,0 +1,35 @@
+import Box from './components/Box';
+import { StyleSheet, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View styles={[styles.container, styles.flexRow]}>
+      <Box color={'red'} size={100}></Box>
+      <Box color={'green'} size={100}></Box>
+      <Box color={'blue'} size={100}></Box>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'aliceblue',
+    flexColumn: { flexDirection: 'column' },
+    flexRow: { flexDirection: 'row' },
+    flexColumnReverse: { flexDirection: 'column-reverse' },
+    flexRowReverse: { flexDirection: 'row' },
+    justifyStart: { justifyContent: 'flex-start' },
+    justifyCenter: { justifyContent: 'center' },
+    justifyEnd: { justifyContent: 'end' },
+    justifySpaceBetween: { justifyContent: 'space-between' },
+    justifySpaceAround: { justifyContent: 'space-around' },
+    justifySpaceEvenly: { justifyContent: 'space-evenly' },
+    alignStart: { alignItens: 'flex-start' },
+    alignCenter: { alignItens: 'center' },
+    alignEnd: { alignItens: 'flex-end' },
+    alignStretch: { alignItens: 'stretch' },
+  },
+});
+
+export default App;
